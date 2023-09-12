1880badf7993ea09b13925b31e99bd31ea5f5960
thiagoferreira <thiagoqatestes@gmail.com>
2023-09-12 14:51:32 -0300

    Configure cypress grep

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,10 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
+    },
     requestTimeout: 12000,
   },
   projectId: 'ioi2eh',
